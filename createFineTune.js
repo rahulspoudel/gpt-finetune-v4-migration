44cad52612c03e836c32dd12fcaa8a877f4741fd
rahulspoudel <13036329+rahulspoudel@users.noreply.github.com>
2023-08-25 20:19:55 +0545

    create finetune mod for sdk v4,listfinetunes added

--- createFineTune.js
+++ createFineTune.js
@@ -2,13 +2,14 @@ import { openai } from './api.js';
 
 async function createFineTune() {
   try {
-    const response = await openai.fineTunes.create({
+    const response = await openai.fineTuning.jobs.create({
       training_file: 'file-7lK24aTyXRVc6dwRsbaerz7S',
-      model: 'davinci',
+      // can use babbage or davinci here
+      model: 'davinci-002',
     });
     console.log('response: ', response);
   } catch (err) {
-    console.log('error: ', err.response.data.error);
+    console.log('error: ', err);
   }
 }
 
--- listFineTunes.js
+++ listFineTunes.js
@@ -2,8 +2,10 @@ import { openai } from './api.js';
 
 async function listFineTunes() {
   try {
-    const response = await openai.fineTunes.list();
-    console.log('data: ', response.data);
+    const response = await openai.fineTuning.jobs.retrieve(
+      'ftjob-BcTfL5i6cIxlnqMz9HODQ6Vy',
+    );
+    console.log('data: ', response);
   } catch (err) {
     console.log('error:', err);
   }

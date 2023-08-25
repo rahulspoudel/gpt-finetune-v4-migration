1962259d9319d2447bf0d23545e7f7d6f0099db2
rahulspoudel <13036329+rahulspoudel@users.noreply.github.com>
2023-08-25 19:32:55 +0545

    running node createFineTune.js to create Fine Tune

--- createFineTune.js
+++ createFineTune.js
@@ -2,8 +2,8 @@ import { openai } from './api.js';
 
 async function createFineTune() {
   try {
-    const response = await openai.createFineTune({
-      training_file: 'file-v9VCTfu5EwFcBnjRhSvePVkq',
+    const response = await openai.fineTunes.create({
+      training_file: 'file-7lK24aTyXRVc6dwRsbaerz7S',
       model: 'davinci',
     });
     console.log('response: ', response);
